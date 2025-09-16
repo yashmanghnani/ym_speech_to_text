fad79ed5bf75d8534800f8ebecb94f3702fb0e04
Yash Manghnani <abcom@Yashs-MacBook-Pro.local>
2025-09-16 11:36:45 +0530

    initial

---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<ym_speech_to_text/SpeechToTextPlugin.h>)
+#import <ym_speech_to_text/SpeechToTextPlugin.h>
+#else
+@import ym_speech_to_text;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [SpeechToTextPlugin registerWithRegistrar:[registry registrarForPlugin:@"SpeechToTextPlugin"]];
+}
+
+@end
